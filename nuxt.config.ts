24d02083a9cc46f26469c5b0f463d743bc2b800b
r00tmeister <117168764+r00tmeister@users.noreply.github.com>
2024-03-28 01:31:44 +0200

    chore: Add Pinia, Tailind, EsLint, Test Utils and VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/eslint-module",
+    "@vueuse/nuxt",
+    "@nuxt/test-utils"
+  ]
 })
